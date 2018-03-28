a9ec94842402e41d7c25cc4fc6cfd4b1ccc04889
LK Loh <lkloh2410@gmail.com>
2018-03-28 06:08:49 -0700

    random forests

--- housing-prices/random_forests.py
+++ housing-prices/random_forests.py
@@ -1,4 +1,5 @@
 import pandas
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -21,14 +22,11 @@ predictors = [
 x = data[predictors]
 
 train_x, validate_x, train_y, validate_y = train_test_split(x, y,random_state = 0)
+model = RandomForestRegressor()
+model.fit(train_x, train_y)
+predicted_y = model.predict(validate_x)
+mae = mean_absolute_error(validate_y, predicted_y)
+print mae
 
-def get_mae(max_leaf_nodes, train_x, validate_x, train_y, validate_y):
-  model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
-  model.fit(train_x, train_y)
-  predicted_prices = model.predict(validate_x)
-  return mean_absolute_error(validate_y, predicted_prices)
+print 'MAE about 23500, less than when using RandomTree'
 
-for max_leaf_nodes in [5, 50, 500, 5000]:
-  print 'mae for %d is %d' % (max_leaf_nodes, get_mae(max_leaf_nodes, train_x, validate_x, train_y, validate_y))
-
-print 'best mae: 50 leafs, ~28000'
